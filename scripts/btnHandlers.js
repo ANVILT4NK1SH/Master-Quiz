1ba9e8bdd102aa222c97ef6190bc0bb5baced4f1
Shawn <iamshawnhartline@gmail.com>
2025-02-13 16:15:55 -0600

    Created more pages/working on pages.

---
+++ scripts/btnHandlers.js
@@ -0,0 +1,26 @@
+import { Biology } from "../data/biologyData";
+import { Chemistry } from "../data/chemistryData";
+import { ComputerScience } from "../data/computerScienceData";
+import { Economics } from "../data/economicsData";
+import { Geography } from "../data/geographyData";
+import { History } from "../data/historyData";
+import { Literature } from "../data/literatureData";
+import { Mathematics } from "../data/mathematicsData";
+import { Physics } from "../data/physicsData";
+import { Science } from "../data/scienceData";
+
+export const easyBtnHandler = () => {
+  window.location.href = '../html/quiz.html'
+};
+
+export const mediumBtnHandler = () => {
+  window.location.href = "../html/quiz.html";
+};
+
+export const hardBtnHandler = () => {
+  window.location.href = "../html/quiz.html";
+};
+
+export const quizSubmitBtnHandler = () => {
+  
+}
--- scripts/initialize.js
+++ scripts/initialize.js
@@ -1,4 +1,5 @@
 import { showSubjects } from "./triviaCards.js";
 import { quizzes } from "../data/quizData.js";
 
+
 showSubjects(quizzes);
